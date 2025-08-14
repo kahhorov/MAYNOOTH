1900f302bd2b59be24aa3538029506d3fe1dc114
kahhorov <sherjahonkahhorov>
2025-08-14 18:54:50 +0500

    sayit tayyor

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const wrapper = document.querySelector(".sideBar__wrapper");
 const bar = document.querySelector(".side__bar");
+const filters = document.querySelector(".filter");
+const filterBox = document.querySelector(".filter_box");
 
 const menuBtn = () => {
   wrapper.style.transform = " translateX(0%)";
@@ -15,3 +17,18 @@ const closeBtn = () => {
   wrapper.style.transform = " translateX(-100%)";
   bar.style.transform = " translateX(-100%)";
 };
+
+const filter = () => {
+  filters.style.transform = " translateX(0%)";
+  wrapper.style.transform = " translateX(-100%)";
+  bar.style.transform = " translateX(-100%)";
+  filterBox.style.display = "flex";
+};
+const filterClose = () => {
+  filters.style.transform = " translateX(-100%)";
+  filterBox.style.display = "none";
+};
+filterBox.addEventListener("click", function () {
+  filters.style.transform = " translateX(-100%)";
+  filterBox.style.display = "none";
+});
